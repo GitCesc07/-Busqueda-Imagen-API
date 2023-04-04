44d9013173051aa488a612086722d83e648ab91f
CescDevGit07 <cesar04071997@gmail.com>
2023-04-03 21:57:15 -0600

    Alerta se debe de mostrar una vez no más, haciendo que el formulario se adapte a movil

--- js/app.js
+++ js/app.js
@@ -17,19 +17,24 @@ function validarFormulario(e) {
 }
 
 function mostrarAlerta(mensaje) {
-  const alerta = document.createElement("p");
-  alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded",
-    "max-w-lg", "mx-auto", "mt-6", "text-center");
 
-  alerta.innerHTML = `
+  const existeAlerta = document.querySelector(".bg-red-100");
+
+  if (!existeAlerta) {
+    const alerta = document.createElement("p");
+    alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded",
+      "max-w-lg", "mx-auto", "mt-6", "text-center");
+
+    alerta.innerHTML = `
     <strong class="font-bold">¡Vaya, ha ocurrido un Error!</strong>
      <br/>
     <span class="block sm:inline">${mensaje}</span>
   `;
 
-  formulario.appendChild(alerta);
+    formulario.appendChild(alerta);
 
-  setTimeout(() => {
-    alerta.remove();
-  }, 3000);
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
 }
