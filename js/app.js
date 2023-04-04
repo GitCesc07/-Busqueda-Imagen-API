ec218d2bb9e3c563b22b75ff4ae210338382c6ff
CescDevGit07 <cesar04071997@gmail.com>
2023-04-03 21:42:18 -0600

    Agregando la alerta al HTML, validar que este vacío el campo de busqueda

--- js/app.js
+++ js/app.js
@@ -11,7 +11,25 @@ function validarFormulario(e) {
   const terminoBusqueda = document.querySelector("#termino").value;
 
   if (terminoBusqueda === "") {
-    console.log("Agrega un término de búsqueda");
+    mostrarAlerta("Agregar un termino, para realizar la busqueda...");
     return;
   }
+}
+
+function mostrarAlerta(mensaje) {
+  const alerta = document.createElement("p");
+  alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded",
+    "max-w-lg", "mx-auto", "mt-6", "text-center");
+
+  alerta.innerHTML = `
+    <strong class="font-bold">¡Vaya, ha ocurrido un Error!</strong>
+     <br/>
+    <span class="block sm:inline">${mensaje}</span>
+  `;
+
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 3000);
 }
